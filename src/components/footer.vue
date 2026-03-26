a54f09d62c9bcb310c50a1625f47c7fc3640df4b
camillobia <beatriz.lombardo2002@gmail.com>
2026-03-26 20:54:06 -0300

    update footer

---
+++ src/components/footer.vue
@@ -0,0 +1,186 @@
+<template>
+  <footer class="footer">
+    <div class="footer__content">
+    <div class="footer__brand">
+    <div class="footer__brand-top">
+    <div class="logo-icon">
+    <img src="../assets/logo_icone.png" alt="Logo da Empresa" />
+    </div>
+
+    <h2 class="footer_logo-text">
+         System<span class="EPI">EPI</span>
+        </h2>
+        </div>
+        <p>Sistema Inteligente para controle <br />
+          e gestão de Equipamentos <br />
+          de Proteção Individual
+        </p>
+      </div>
+
+      <div class="footer__colunas">
+        <h4>Sistema</h4>
+        <a href="#">Controle de EPIs</a>
+        <a href="#">Estoque</a>
+        <a href="#">Validade</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Relatórios</a>
+      </div>
+
+      <div class="footer__colunas">
+        <h4>Funcionalidades</h4>
+        <a href="#">Rastreabilidade</a>
+        <a href="#">Histórico de entregas</a>
+        <a href="#">Gestão de colaboradores</a>
+        <a href="#">Segurança do trabalho</a>
+      </div>
+
+      <div class="footer__colunas">
+        <h4>Suporte</h4>
+        <a href="#">Ajuda</a>
+        <a href="#">Contato</a>
+        <a href="#">Suporte</a>
+        <a href="#">FAQ</a>
+      </div>
+
+      <div class="footer__colunas">
+        <h4>Redes sociais</h4>
+
+        <div class="footer_icones">
+          <a href="https://www.instagram.com/" class="footer__link">
+            <img src="../assets/icone_instagram.png" alt="icone instagram" />
+          </a>
+
+          <a href="https://pt-br.facebook.com/" class="footer__link">
+            <img src="../assets/icone_facebook.png" alt="icone facebook" />
+          </a>
+
+          <a href="https://br.linkedin.com/" class="footer__link">
+            <img src="../assets/icone_linkedin.png" alt="icone linkedin" />
+          </a>
+
+          <a href="https://web.whatsapp.com/" class="footer__link">
+            <img src="../assets/icone_whatsapp.png" alt="icone whatsapp" />
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer_copy">
+      <p>© 2026 System EPI. Todos os direitos reservados.</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background: #e8e8ee;
+  padding: 3rem 4rem 1rem;
+}
+
+.footer__content {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 3.5rem;
+  margin-bottom: 2rem;
+}
+
+.footer__brand {
+  max-width: 240px;
+}
+
+.footer__brand-top {
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  margin-bottom: 0.6rem;
+}
+
+.logo-icon img {
+  width: 50px;
+  height: 25px;
+  object-fit: contain;
+  transform: rotate(-12deg) translate(-24px, 3px);
+}
+
+.footer_logo-text {
+  font-size: 1.2rem;
+  font-family: poppins, sans-serif;
+  font-weight: semi-bold;
+  color: #0e2238;
+  position: relative;
+  top: 22px;
+  left: -57px;
+}
+
+.footer_logo-text .EPI {
+  color: #f5b301;
+  margin-left: 3px;
+}
+
+.footer__brand p {
+  font-size: 0.7rem;
+  line-height: 1.5;
+  font-family: poppins, sans-serif;
+  font-weight: semi-bold;
+  color: #0e2238;
+   position: relative;
+  top: 9px;
+  left: 1px;
+}
+
+.footer__colunas {
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  min-width: 150px;
+}
+
+.footer__colunas h4 {
+  font-size: 0.9rem;
+  font-family: poppins, sans-serif;
+  font-weight: semi-bold;
+  color: #0e2238;
+
+}
+
+.footer__colunas a {
+  font-size: 0.9rem;
+  font-family: poppins, sans-serif;
+  color: #0e2238;
+  text-decoration: none;
+  transition: 0.2s ease;
+}
+
+.footer__colunas a:hover {
+  opacity: 0.7; 
+}
+
+.footer_icones {
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  margin-top: 0.3rem;
+}
+
+.footer_icones img {
+  width: 28px;
+  height: 28px;
+  object-fit: contain;
+}
+
+.footer_copy {
+  width: 100%;
+  text-align: center;
+  border-top: 1px solid #0e2238;
+  padding-top: 1rem;
+}
+
+.footer__copy p {
+  margin: 0;
+  color: #0e2238;
+  font-size: 0.8rem;
+  font-family: poppins, sans-serif;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -27,7 +27,7 @@
         <div class="card_controle">                                                               
            <div>  
             <span class="card_icone">
-                 <img src="../assets/card_1.png" alt="icone de EPI";>
+                 <img src="../assets/icone_card1.png" alt="icone package";>
             </span>                                       
            </div>
          <h2>Controle de Estoque</h2>
@@ -35,15 +35,21 @@
            </p>
         </div>                                                                       
 
-        <div class="card_registro">                 
-          <span class="card_icone"></span>                                       
+        <div class="card_registro"> 
+           <div>  
+            <span class="card_icone">
+                 <img src="../assets/icone_card2.png" alt="icone calendario";>
+            </span>                                       
+           </div>
           <h2>Registro de Entregas</h2>    
           <p>Registre e acompanhe a entrega de equipamento de proteção individual para cada colaborador</p>
           </div>
-        
-        
            <div class="card_gestao">                 
-          <span class="card_icone"></span>                                       
+           <div>  
+            <span class="card_icone">
+                 <img src="../assets/icone_card3.png" alt="icone boneco";>
+            </span>                                       
+           </div>
           <h2>Gestão de Usuários</h2>    
           <p>Cadastre colaboradores e gerencie permisões de acessoao sistema de gestão de EPIs
            </p>  
@@ -51,13 +57,19 @@
       </div>
       <div class="card-segundo">      
         <div class="card_relatorios">                 
-          <span class="card__icone"></span>                                       
+           <div>  
+            <span class="card_icone2">
+                 <img src="../assets/icone_card4.png" alt="icone grafico">
+            </span>                                       
+           </div>
           <h2>Relatórios</h2>    
           <p>Visualize relatórios e dados importantes para acompanhar o uso e controles dos EPIs
            </p>
            </div>
            <div class="card_rastreabilidade">
-          <span class="card__icone"></span>                                       
+            <span class="card_icone3">
+            <img src="../assets/icone_card5.png" alt="icone rastreabilidade">
+            </span>                                       
           <h2>Rastreabilidade</h2>    
           <p>Visualize relatórios e dados importantes para acompanhar o uso e controles dos EPIs
            </p>
@@ -65,27 +77,55 @@
            </div>                                                               
     </section>
   </div>                                                                           
-
-</template>                                                                        <!-- fecha o template -->
+  <Footer/>
+</template>                                                                        
 <script setup>
 import Header from '../components/header.vue';
+import Footer from '../components/footer.vue';
 </script>
 
 <style scoped>  
 
 
 .home {                                                                 
-  background: #E8E8EE;                                                             /* fundo cinza claro */
+  background: #e8e8ee;
 }
+
+
 .home__hero {
-  display: flex;                                                                   /* texto e imagem lado a lado */
-  align-items: flex-start;                                                         /* alinha no topo */
-  gap: 2rem;                                                                       /* espaço entre texto e imagem */
-  margin-bottom:15rem;                                                             /* espaço abaixo da seção */
+  display: flex;                                                                  
+  align-items: flex-start;                                                         
+  gap: 2rem;                                                                       
+  margin-bottom:15rem;     
+   background:radial-gradient(circle, #e8e8ee 0%, #E1EBEE 70%, #0e2238 100%);
+   position: relative;
+   overflow: hidden;   
+   padding: 50px 0rem 65px 2rem;                                                   
+}
+
+.home__hero::before {
+  content: "";
+  position: absolute;
+  top: 20px;
+  left: -120px;
+  width: 900px;
+  height: 300px;
+  background: rgba (255,255, 255, 0.22);
+  border-radius: 80px;
+}
+
+.home__hero::after {
+ position: absolute;
+  width: 950px;
+  height: 280px;
+  background: rgba(190,210,230,0.18);
+  right: -120px;
+  border-radius: 80px;
+
 }
 
 .home_texto-titulo { 
-  font-size: 26px;
+  font-size: 1.9rem;
   font-family: poppins, sans-serif;
   font-weight: semi-bold;
   text-align: left;
@@ -95,8 +135,8 @@ import Header from '../components/header.vue';
 }
 
 .home_texto-titulo p {
-  font-size: 20px;
-}/* ocupa metade do espaço */
+  font-size: 1.25rem;
+}
               
 
 .landing_page-imagem { 
@@ -104,32 +144,32 @@ import Header from '../components/header.vue';
   display: flex;
   justify-content: right;
   align-items: center;
-  margin-right: 3rem;
-  margin-top: 1rem;
+  margin-right: 5rem;
+  margin-top: 3rem;
 }
 
 .landing_page-imagem img {
-  width: 45rem;
+  width: 40rem;
   max-width: 100rem;
   height: auto;
-  border-radius: 10px;
+  border-radius: 1rem;
 }
 
 
-/* estilo do <p> dentro de .home__hero-texto */
+
 .home__hero-texto p {
-  font-size: 20px;                                                                 /* tamanho do texto */
-  color: #555;                                                                     /* cinza médio */
-  line-height: 1.7;                                                                /* espaço entre linhas */
+  font-size: 1.25rem;                                                                 
+  color: #555;                                                                     
+  line-height: 1.7;                                                               
 }
 
-/* estilo do <img class="home__hero-img"> */
+
 .home__hero-img {
-  width: 100%;                                                                     /* ocupa toda a largura do bloco */
-  border-radius: 12px;                                                             /* cantos arredondados */
+  width: 100%;                                                                    
+  border-radius: 12px;                                                             
 }
 
-/* estilo do <section class="home__cards"> */
+
 .home__cards {
   display: flex;                                                                  
   flex-direction: column;
@@ -140,10 +180,10 @@ import Header from '../components/header.vue';
 .cards-row {
   display: flex;
   flex-direction: row;
-  gap: 5rem;
+  gap: 10rem;
 }
 
-/* estilo do <div class="card"> */
+
 .card_controle {
   display: flex;                                                                   
   flex-direction: column;                                                          
@@ -158,17 +198,19 @@ import Header from '../components/header.vue';
                                                          
 }
 
+
 .card_controle h2 {
-  font-size: 20px;
+  font-size: 22px;
   font-family: poppins;
   font-weight: semi-bold;
   text-align: center;
   color: #0e2238;
-  margin-bottom: 1rem;
+  margin-top: 2rem;
+  margin-bottom: 1.9rem;;
 }
 
 .card_controle p {
-  font-size: 1.0rem;
+  font-size: 0.93rem;
   text-align: center;
   color: #0e2238 ;
 }
@@ -182,20 +224,22 @@ import Header from '../components/header.vue';
   border: 1px solid #E2E8F0;                                                      
   border-radius: 12px;    
   width: 12rem;
-  height: 16rem;                                                           
+  height: 16rem;
+  position: relative;                                                          
 }
 
 .card_registro h2 {
-  font-size: 20px;
+  font-size: 22px;
   font-family: poppins;
   font-weight: semi-bold;
   text-align: center;
   color: #0e2238;
-  margin-bottom: 1rem;
+  margin-top: 2rem;
+  margin-bottom: 1.9rem;;
 }
 
 .card_registro p {
-  font-size: 1.0rem;
+  font-size: 0.94rem;
   text-align: center;
   color: #0e2238 ;
 }
@@ -209,7 +253,8 @@ import Header from '../components/header.vue';
   border: 1px solid #E2E8F0;                                                      
   border-radius: 12px;    
   width: 12rem;
-  height: 16rem;                                                           
+  height: 16rem;
+  position: relative;                                                          
 }
 
 .card_gestao h2 {
@@ -218,11 +263,12 @@ import Header from '../components/header.vue';
   font-weight: semi-bold;
   text-align: center;
   color: #0e2238;
+  margin-top: 2rem;
   margin-bottom: 1rem;
 }
 
 .card_gestao p {
-  font-size: 1.0rem;
+  font-size: 0.93rem;
   text-align: center;
   color: #0e2238 ;
 }
@@ -263,7 +309,8 @@ import Header from '../components/header.vue';
   border: 1px solid #E2E8F0;                                                      
   border-radius: 12px;    
   height: 16rem;
-  width: 12rem;                                             
+  width: 12rem;   
+                                       
 }
 
 .card_rastreabilidade h2 {
@@ -296,16 +343,84 @@ import Header from '../components/header.vue';
 .card_icone {
   display: flex;
   flex-direction: column;
-  width: 50px;
-  height: 50px;
+  width: 45px;
+  height: 45px;
   background-color: #0e2238;
   border: 1px solid #E2E8F0;   
   justify-content: center;
   align-items: center;
   position: absolute;
-  top: -25px;
+  top: -22px;
   left: 50%;
-  transform: translateX(-50%) rotate(-50deg);
+  transform: translateX(-50%) rotate(-46deg);
+   border: 1px solid #E2E8F0;   
+   border-radius: 0.3rem; 
+}
+
+.card_controle .card_icone img {
+  width: 24px;
+  height: 24px;
+  transform: rotate(45deg);
+}
+
+.card_registro .card_icone img {
+  width: 24px;
+  height: 24px;
+  transform: rotate(45deg);
+}
+
+.card_gestao .card_icone img {
+  width: 24px;
+  height: 24px;
+  transform: rotate(45deg);
+}
+
+
+
+.card_icone2 {
+  display: flex;
+  flex-direction: column;
+  width: 45px;
+  height: 45px;
+  background-color: #0e2238;
+  border: 1px solid #E2E8F0;   
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: -46px;
+  left: 50%;
+  transform: translateX(-50%) rotate(-46deg);
+   border: 1px solid #E2E8F0;   
+   border-radius: 0.3rem; 
+}
+  
+.card_relatorios .card_icone2 img {
+  width: 24px;
+  height: 24px;
+  transform: rotate(45deg);
+}
+
+.card_rastreabilidade .card_icone3 img {
+  width: 20px;
+  height: 20px;
+  transform: rotate(45deg);
+}
+
+.card_icone3 {
+  display: flex;
+  flex-direction: column;
+  width: 45px;
+  height: 45px;
+  background-color: #0e2238;
+  border: 1px solid #E2E8F0;   
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: -46px;
+  left: 1.5rem;;
+  transform: translateX(-50%) rotate(-46deg);
+   border: 1px solid #E2E8F0;   
+   border-radius: 0.3rem; 
 }
 
 </style>                                                                            
